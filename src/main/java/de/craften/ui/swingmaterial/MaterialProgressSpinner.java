b7d244e39e9898bca38bae13c7b445aaee0eb3e3
Maik Marschner <an@maik.tk>
2016-03-29 14:06:53 +0200

    Make progress spinner inherit from JComponent only.

--- src/main/java/de/craften/ui/swingmaterial/MaterialProgressSpinner.java
+++ src/main/java/de/craften/ui/swingmaterial/MaterialProgressSpinner.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
  *
  * @see <a href="https://www.google.com/design/spec/components/progress-activity.html">Progress &amp; activity (Google design guidelines)</a>
  */
-public class MaterialProgressSpinner extends JPanel {
+public class MaterialProgressSpinner extends JComponent {
     private static final int DURATION = 1200;
     private int startArc = 270;
     private int arcSize = 0;
@@ -97,11 +97,10 @@ public class MaterialProgressSpinner extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(getForeground());
         g2.setStroke(new BasicStroke(5, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-        g2.drawArc(5, 5, 50 - 10, 50 - 10, startArc + rotation + rotation2 + 90, arcSize);
+        g2.drawArc(5, 5, getWidth() - 10, getWidth() - 10, startArc + rotation + rotation2 + 90, arcSize);
     }
 }
