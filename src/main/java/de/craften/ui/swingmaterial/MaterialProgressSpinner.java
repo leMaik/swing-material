74d98e84e624d5675513764451a72a91a862a44a
Maik Marschner <an@maik.tk>
2016-04-20 10:50:27 +0200

    Fix jittering progress spinner.

--- src/main/java/de/craften/ui/swingmaterial/MaterialProgressSpinner.java
+++ src/main/java/de/craften/ui/swingmaterial/MaterialProgressSpinner.java
@@ -99,7 +99,15 @@ public class MaterialProgressSpinner extends JComponent {
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+
         g2.setColor(getForeground());
         g2.setStroke(new BasicStroke(5, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_ROUND));
         g2.drawArc(5, 5, getWidth() - 10, getWidth() - 10, startArc.getValue() + rotation.getValue() + 90, Math.max(1, arcSize.getValue()));
