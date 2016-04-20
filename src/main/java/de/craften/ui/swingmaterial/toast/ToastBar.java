11bba5609e61271853ff32b2b41a1d393b2d6e19
Maik Marschner <an@maik.tk>
2016-04-20 11:33:41 +0200

    Fix some bugs and rare NPEs when displaying toasts.

--- src/main/java/de/craften/ui/swingmaterial/toast/ToastBar.java
+++ src/main/java/de/craften/ui/swingmaterial/toast/ToastBar.java
@@ -7,10 +7,7 @@ import org.jdesktop.core.animation.timing.interpolators.SplineInterpolator;
 import org.jdesktop.swing.animation.timing.sources.SwingTimerTimingSource;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.event.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.TimeUnit;
@@ -23,22 +20,12 @@ import java.util.concurrent.TimeUnit;
 public class ToastBar extends JComponent {
     private final SwingTimerTimingSource timer;
     private Queue<Toast> toasts = new LinkedList<>();
-    private Toast currentToast = null;
     private boolean animationRunning = false;
 
     public ToastBar() {
         setLayout(null);
         timer = new SwingTimerTimingSource();
         timer.init();
-
-        addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                if (currentToast != null) {
-                    currentToast.setSize(getWidth(), currentToast.getHeight());
-                }
-            }
-        });
     }
 
     /**
@@ -48,67 +35,79 @@ public class ToastBar extends JComponent {
      */
     public void display(Toast toast) {
         toasts.add(toast);
-        if (toasts.size() == 1) {
+        if (toasts.size() == 1 && !animationRunning) {
             displayNextToast();
         }
     }
 
     @Override
     public boolean contains(int x, int y) {
-        if (currentToast != null) {
-            return super.contains(x, y);
-        } else {
-            return false;
-        }
+        return animationRunning && super.contains(x, y);
     }
 
-    private void displayNextToast() {
+    private synchronized void displayNextToast() {
         if (animationRunning) {
             return;
         }
 
-        currentToast = toasts.poll();
+        final Toast currentToast = toasts.poll();
         if (currentToast != null) {
             add(currentToast);
             currentToast.setSize(getWidth(), 48);
 
+            final ComponentListener resizeListener = new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    currentToast.setSize(getWidth(), currentToast.getHeight());
+                    repaint();
+                }
+            };
+            addComponentListener(resizeListener);
+
             animationRunning = true;
             new Animator.Builder(timer)
                     .setDuration(250, TimeUnit.MILLISECONDS)
-                    .setEndBehavior(Animator.EndBehavior.HOLD)
                     .setInterpolator(new SplineInterpolator(0.1, 0.3, 0.45, 1))
                     .addTarget(SafePropertySetter.getTarget(new SafePropertySetter.Setter<Integer>() {
                         @Override
                         public void setValue(Integer value) {
-                            currentToast.setYOffset(value);
+                            if (value != null) {
+                                currentToast.setYOffset(value);
+                            }
                             repaint();
                         }
                     }, getHeight(), 0))
+                    .addTarget(new TimingTargetAdapter() {
+                        @Override
+                        public void end(Animator source) {
+                            currentToast.setYOffset(0);
+                        }
+                    })
                     .build().start();
+
             new Animator.Builder(timer)
                     .setStartDelay(250 + 3000, TimeUnit.MILLISECONDS)
                     .setDuration(250, TimeUnit.MILLISECONDS)
-                    .setEndBehavior(Animator.EndBehavior.HOLD)
                     .setInterpolator(new SplineInterpolator(0.55, 0, 0.9, 0.7))
                     .addTarget(SafePropertySetter.getTarget(new SafePropertySetter.Setter<Integer>() {
                         @Override
                         public void setValue(Integer value) {
-                            currentToast.setYOffset(value);
+                            if (value != null) {
+                                currentToast.setYOffset(value);
+                            }
                             repaint();
                         }
                     }, 0, getHeight() + 1))
                     .addTarget(new TimingTargetAdapter() {
                         @Override
                         public void end(Animator source) {
-                            if (currentToast != null) {
-                                remove(currentToast);
-                                currentToast = null;
-                            }
+                            removeComponentListener(resizeListener);
+                            remove(currentToast);
 
+                            animationRunning = false;
                             new javax.swing.Timer(1000, new ActionListener() {
                                 @Override
                                 public void actionPerformed(ActionEvent e) {
-                                    animationRunning = false;
                                     displayNextToast();
                                 }
                             }).start();
